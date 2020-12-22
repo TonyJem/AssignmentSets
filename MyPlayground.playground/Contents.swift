1ad410f8b1780690a2ca8579618421165569a12c
TonyJem <a.jemanov@gmail.com>
2020-12-22 13:21:16 +0200

    Start own solution

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+struct ReceiptItem {
+    let name: String
+    var price: Double
+    var quantity: Int
+    var salePrice: Double?
+}
 
-var str = "Hello, playground"
+var chocolate = ReceiptItem(name: "Chocolate", price: 15.25, quantity: 10, salePrice: nil)
+var candy = ReceiptItem(name: "Candy", price: 13.01, quantity: 5, salePrice: nil)
+var cake = ReceiptItem(name: "Cake", price: 9.78, quantity: 2, salePrice: nil)
+var spurga = ReceiptItem(name: "Spurga", price: 0.18, quantity: 7, salePrice: 0.19)
+
+
+
+
+
+
